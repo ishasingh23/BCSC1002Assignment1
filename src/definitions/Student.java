8803cc4dee9b92cdae45a51c7ae909f075b2f4da
ISHA SINGH <isha.singh_cs19@gla.ac.in>
2020-08-25 16:48:57 +0530

    add method in Student.java to add student name,university roll no,number of bool issued,names of book issued

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -6,5 +6,24 @@
  * */
 package definitions;
 
+import java.util.Arrays;
+
 public class Student {
+    private Book[] namesOfBookIssued;
+
+    public void studentName(String firstName, String middleName, String lastName) {
+        System.out.println("Name of the number " + firstName + " " + middleName + "" + lastName);
+    }
+
+    public void universitRollNumber(long rollNumber) {
+        System.out.println("student university roll number :" + rollNumber);
+    }
+
+    public void numberOfBookIssued(int numberOfBooks) {
+        System.out.println("number of book issued bu students: " + numberOfBooks);
+    }
+
+    public void namesOfBookIssued(Book[] namesOfBookIssued) {
+        System.out.println("names of book issued: " + Arrays.toString(namesOfBookIssued));
+    }
 }
